4dd4c45494ca9190df61779d6ecfdb476128a5da
FortressTechnologiesInc <130385349+FortressTechnologiesInc@users.noreply.github.com>
2026-03-04 14:24:39 -0500

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,19 +3,22 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav style={{
-      backgroundColor: "#b8860b",
-      padding: "15px",
-      display: "flex",
-      justifyContent: "space-around",
-      color: "white",
-      fontWeight: "bold"
-    }}>
-      <Link to="/dashboard">Dashboard</Link>
-      <Link to="/transactions">Transactions</Link>
-      <Link to="/transfer">Transfer</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
+    <nav
+      style={{
+        backgroundColor: "#b8860b",
+        padding: "15px",
+        display: "flex",
+        justifyContent: "space-around",
+        color: "white",
+        fontWeight: "bold",
+        fontSize: "18px"
+      }}
+    >
+      <Link style={{ color: "white", textDecoration: "none" }} to="/dashboard">Dashboard</Link>
+      <Link style={{ color: "white", textDecoration: "none" }} to="/transactions">Transactions</Link>
+      <Link style={{ color: "white", textDecoration: "none" }} to="/transfer">Transfer</Link>
+      <Link style={{ color: "white", textDecoration: "none" }} to="/login">Login</Link>
+      <Link style={{ color: "white", textDecoration: "none" }} to="/register">Register</Link>
     </nav>
   );
 }
